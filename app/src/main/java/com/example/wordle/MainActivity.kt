b8d15fbbe9fbd84db3610ad8fa806b59f6145ddc
A-Abra <apabraham472@gmail.com>
2023-01-29 23:17:53 -0500

    Implemented 3/4 main components.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -15,7 +15,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+        var guessOne = findViewById<TextView>(R.id.guessOne)
+        var checkOne = findViewById<TextView>(R.id.checkOne)
+        var guessTwo = findViewById<TextView>(R.id.guessTwo)
+        var checkTwo = findViewById<TextView>(R.id.checkTwo)
+        var guessThree = findViewById<TextView>(R.id.guessThree)
+        var checkThree = findViewById<TextView>(R.id.checkThree)
+        val ansView = findViewById<TextView>(R.id.answer)
+        val guessInput = findViewById<Button>(R.id.guessBtn)
+        val editText = findViewById<EditText>(R.id.userInput)
 
+        var guesses = 3
+
+        guessInput.setOnClickListener {
+            // reset game
+            if (guesses == 0){
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
+
+            // guesses remaining conditional
+            if (guesses != 0) {
+                var correctGuess = false
+                val editTextString = editText.text.toString().uppercase(Locale.getDefault())   // to string for edit text
+                if (editTextString.length == 4) {
+                    ansView.text = ""
+                    val result = checkGuess(editTextString, wordToGuess)
+                    checkGuess(editTextString, wordToGuess)
+
+                    // guessed right
+                    if (result == "OOOO") {
+                        ansView.text = "Correct! Answer: $wordToGuess"
+                        guessInput.text = "Reset"
+                        correctGuess = true
+                        guesses = 0
+                    } else {
+                        guesses--
+
+                        if (guesses == 0) {
+                            ansView.text = "Answer: $wordToGuess"
+                            guessInput.text = "Reset"
+                        }
+                    }
+                }
+
+                else
+                    ansView.text = "Enter a Four Letter Word Only"
+            }
+        }
     }
 
     /**
